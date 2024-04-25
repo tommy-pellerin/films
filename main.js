63fcc3989941ad5f6db4b3facda5d74a034770aa
tommy-pellerin <tommytp@hotmail.fr>
2024-04-25 13:41:40 +0200

    details shown

--- main.js
+++ main.js
@@ -1,34 +1,65 @@
 import { showMovies } from './results.js';
+import { showDetails } from './results.js';
 
-let url;
-url = 'temp.json'
 const moviesBox = document.getElementById('moviesBox');
 const form = document.querySelector('form');
 const film = document.getElementById('search');
 let filmValue;
+let url;
 
-film.addEventListener('input',()=>{
-  console.log(film.value);
-})
+// film.addEventListener('input',()=>{
+//   console.log(film.value);
+// })
 
 form.addEventListener('submit', function(event) {
   // Prevent the form from submitting normally
   event.preventDefault();
   filmValue = film.value;
+  
+  if (filmValue){
+    url = 'temp.json'
+  };
   // Fetch data immediately
-
-  fetchData();
+  fetchData(url);
 });
 
-async function fetchData() {
+//fetching first information part
+async function fetchData(url) {
   try {
     const response = await fetch(url);
     const results = await response.json();
     console.log(results);
     moviesBox.innerHTML = ''
-    showMovies(results,moviesBox);
+    // Assuming showMovies can handle an array of movies
+    results.Search.forEach((movie,index) => {
+      
+      showMovies(movie, moviesBox, index);
+    });
   } catch (error) {
     console.error('Response error:', error.message);
   }
 }
-fetchData();
+fetchData('temp.json');
+
+//fetching details part
+export async function fetchDetail(detailUrl) {
+  try {
+  const response = await fetch(detailUrl);
+  const result = await response.json();
+  console.log(result);
+    
+  showDetails(result, moviesBox);
+  
+  } catch (error) {
+    console.error('Response error:', error.message);
+  }
+}
+
+// let detailsButton = document.getElementById('details');
+// console.log(detailsButton);
+// detailsButton.addEventListener('click', (event) => {
+//   let index = event.target.getAttribute('data-index');
+//   let movie = results.Search[index];
+//   let detailUrl = 'http://www.omdbapi.com/?apikey=bd77e14&i=' + movie.imdbID;
+//   fetchDetail(detailUrl);
+// });
--- results.js
+++ results.js
@@ -1,11 +1,51 @@
-export const showMovies = (results,moviesBox) => {
-  let title = results.Title;
-  let released = results.Released;
-  let actors = results.Actors;
-  let director = results.Director;
-  let description = results.Plot;
-  let duration = results.Runtime;
-  let imageUrl = results.Poster;
+import { fetchDetail } from './main.js';
+
+export const showMovies = (movie,moviesBox,index) => {
+  // console.log(movie);
+  let title = movie.Title;
+  let year = movie.Year;
+  let imageUrl = movie.Poster;
+  moviesBox.innerHTML += `
+      <div class="movieBox">
+          <div class="poster">
+            <img src="${imageUrl}" alt="poster" />
+          </div>
+          <div class="movieBody">
+            <h2>${title}</h2>
+            <p>Release date : ${year}</p>
+            <button class="details" >READ MORE</button>
+          </div>
+                  
+      </div>
+  `;
+
+  setTimeout(()=> {
+    let detailsButtons = document.querySelectorAll('.details');
+    detailsButtons.forEach((button, buttonIndex) => {
+      if (buttonIndex === index) {
+        button.addEventListener('click', (event) => {
+          console.log("test");
+          // let index = event.target.getAttribute('data-index');
+          console.log(movie);
+          let detailUrl = 'http://www.omdbapi.com/?apikey=bd77e14&i=' + movie.imdbID;
+          fetchDetail(detailUrl);
+        });
+      }
+    });
+  });
+
+}
+
+export const showDetails = (movie,moviesBox) => {
+  console.log("details");
+  console.log(movie);
+  let title = movie.Title;
+  let released = movie.Released;
+  let actors = movie.Actors;
+  let director = movie.Director;
+  let description = movie.Plot;
+  let duration = movie.Runtime;
+  let imageUrl = movie.Poster;
   moviesBox.innerHTML += `
       <div class="movieBox">
           <img src="${imageUrl}" alt="poster" />
